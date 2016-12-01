3e1f37a4466f09b9e24c72509da28a86598be3b9
Greg Koblanski (melquiades) <gregk@opensignal.com>
2016-12-01 21:48:00 +0000

    Add main function

---
+++ src/AdventOfCode.kt
@@ -0,0 +1,21 @@
+import season_2015.*
+
+/**
+ * Created by gregk on 18/10/2016.
+ */
+fun main(args: Array<String>) {
+    var day : BaseDay = Day01(); //day.run()
+
+//    day = Day02(); day.run()
+//    day = Day03(); day.run()
+//    day = Day04(); day.run()
+//    day = Day05(); day.run()
+//    day = Day06(); day.run()
+//    day = Day09(); day.run()
+//    day = Day10(); day.run()
+//    day = Day11(); day.run()
+//    day = Day13(); day.run()
+//    day = Day14(); day.run()
+//    day = Day15(); day.run()
+    day = season_2016.Day01(); day.run()
+}
