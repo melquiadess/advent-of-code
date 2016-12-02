f87c9b3466d2de0d43fd8ac616683e72761ef33f
Greg Koblanski (melquiades) <gregk@opensignal.com>
2016-12-02 00:23:56 +0000

    Get ready for Day02

---
+++ src/season_2016/Day02.kt
@@ -0,0 +1,15 @@
+package season_2016
+
+import BaseDay
+
+/**
+ * Created by melquiadess on 02/12/2016.
+ */
+class Day02 : BaseDay {
+
+    override fun run() {
+        println("Day02")
+
+        println()
+    }
+}
