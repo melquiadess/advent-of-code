f23368f765967fd1fdf6037cfc966debcc0ecf3f
Greg Koblanski (melquiades) <gregk@opensignal.com>
2017-12-27 14:25:30 +0000

    Add Day18

--- src/season_2017/Day18.kt
+++ src/season_2017/Day18.kt
@@ -1,6 +1,7 @@
 package season_2017
 
 import AbstractDay
+import java.util.*
 
 /**
  * Created by melquiadess on 18/12/2017.
@@ -23,6 +24,17 @@ class Day18 : AbstractDay("Day18") {
     var p: Long = 0L
 
     var opIndex = 0
+
+    var a1: Long = 0L
+    var b1: Long = 0L
+    var i1: Long = 0L
+    var f1: Long = 0L
+    var p1: Long = 0L
+
+    var opIndex1 = 0
+
+    var queue1 = ArrayDeque<Instruction>()
+    var queue2 = ArrayDeque<Instruction>()
 
     private fun getInput() {
 //        list = readFile("src/season_2017/input/day18-input-test")
@@ -47,27 +59,27 @@ class Day18 : AbstractDay("Day18") {
         return this.isNotEmpty() && this in "abcdefghijklmnopqrstuvwxyz"
     }
 
-    fun getRegisterValue(register: String): Long = when (register) {
-        "a" -> a
-        "b" -> b
-        "i" -> i
-        "f" -> f
-        "p" -> p
+    fun getRegisterValue(register: String, pid: Int): Long = when (register) {
+        "a" -> if (pid == 0) a else a1
+        "b" -> if (pid == 0) b else b1
+        "i" -> if (pid == 0) i else i1
+        "f" -> if (pid == 0) f else f1
+        "p" -> if (pid == 0) p else p1
         else -> -666
     }
 
-    fun setRegisterValue(register: String, value: Long) {
+    fun setRegisterValue(register: String, value: Long, pid: Int) {
         when (register) {
-            "a" -> a = value
-            "b" -> b = value
-            "i" -> i = value
-            "f" -> f = value
-            "p" -> p = value
+            "a" -> if (pid == 0) a = value else a1 = value
+            "b" -> if (pid == 0) b = value else b1 = value
+            "i" -> if (pid == 0) i = value else i1 = value
+            "f" -> if (pid == 0) f = value else f1 = value
+            "p" -> if (pid == 0) p = value else p1 = value
             else -> println("Can't set register: $register")
         }
     }
 
-    private fun startOperations() {
+    private fun startOperations(pid: Int) {
         var lastFrequencyPlayed: Long = 0L
 
         loop@ while (true) {
@@ -78,7 +90,7 @@ class Day18 : AbstractDay("Day18") {
                 "snd" -> {
                     // snd X plays a sound with a frequency equal to the value of X.
                     println("PLAYING ${op.register}")
-                    lastFrequencyPlayed = getRegisterValue(op.register)
+                    lastFrequencyPlayed = getRegisterValue(op.register, pid)
                     opIndex++
                 }
                 "set" -> {
@@ -89,10 +101,10 @@ class Day18 : AbstractDay("Day18") {
                         value = op.value.toLong()
                     } else {
                         println("Getting value from ${op.value}")
-                        value = getRegisterValue(op.value)
+                        value = getRegisterValue(op.value, pid)
                     }
 
-                    setRegisterValue(op.register, value)
+                    setRegisterValue(op.register, value, pid)
                     opIndex++
                 }
                 "add" -> {
@@ -101,10 +113,10 @@ class Day18 : AbstractDay("Day18") {
                     if (!op.value.isLetter()) {
                         increaseValue = op.value.toLong()
                     } else {
-                        increaseValue = getRegisterValue(op.value)
+                        increaseValue = getRegisterValue(op.value, pid)
                     }
-                    var currentValue = getRegisterValue(op.register)
-                    setRegisterValue(op.register, currentValue + increaseValue)
+                    var currentValue = getRegisterValue(op.register, pid)
+                    setRegisterValue(op.register, currentValue + increaseValue, pid)
                     opIndex++
                 }
                 "mul" -> {
@@ -113,10 +125,10 @@ class Day18 : AbstractDay("Day18") {
                     if (!op.value.isLetter()) {
                         multFactor = op.value.toLong()
                     } else {
-                        multFactor = getRegisterValue(op.value)
+                        multFactor = getRegisterValue(op.value, pid)
                     }
-                    val currentValue = getRegisterValue(op.register)
-                    setRegisterValue(op.register, currentValue * multFactor)
+                    val currentValue = getRegisterValue(op.register, pid)
+                    setRegisterValue(op.register, currentValue * multFactor, pid)
                     opIndex++
                 }
                 "mod" -> {
@@ -126,17 +138,17 @@ class Day18 : AbstractDay("Day18") {
                     if (!op.value.isLetter()) {
                         moduloValue = op.value.toLong()
                     } else {
-                        moduloValue = getRegisterValue(op.value)
+                        moduloValue = getRegisterValue(op.value, pid)
                     }
 
-                    val currentValue = getRegisterValue(op.register)
-                    setRegisterValue(op.register, currentValue.rem(moduloValue))
+                    val currentValue = getRegisterValue(op.register, pid)
+                    setRegisterValue(op.register, currentValue.rem(moduloValue), pid)
                     opIndex++
                 }
                 "rcv" -> {
                     // rcv X recovers the frequency of the last sound played, but only when the value of X is not zero.
                     // (If it is zero, the command does nothing.)
-                    val regValue = getRegisterValue(op.register)
+                    val regValue = getRegisterValue(op.register, pid)
                     println("Value in ${op.register} is $regValue...")
 
                     if (regValue != 0L) {
@@ -152,7 +164,111 @@ class Day18 : AbstractDay("Day18") {
                     if (!op.register.isLetter()) {
                         regValue = op.register.toLong()
                     } else {
-                        regValue = getRegisterValue(op.register)
+                        regValue = getRegisterValue(op.register, pid)
+                    }
+
+                    if (regValue > 0L) {
+                        val offset = op.value.toInt()
+                        opIndex += offset
+                    }
+                    else {
+                        opIndex++
+                    }
+                }
+            }
+
+            printRegisters()
+        }
+
+        println("Last played frequency: $lastFrequencyPlayed")
+    }
+
+    private fun startOperations2(pid: Int) {
+        var lastFrequencyPlayed: Long = 0L
+
+        loop@ while (true) {
+            var op = instructions[opIndex]
+            print("==> operation: ${op.name} ${op.register} ${op.value}  :::  ")
+
+            when (op.name) {
+                "snd" -> {
+                    // snd X plays a sound with a frequency equal to the value of X.
+                    println("PLAYING ${op.register}")
+                    lastFrequencyPlayed = getRegisterValue(op.register, pid)
+                    opIndex++
+                }
+                "set" -> {
+                    // set X Y sets register X to the value of Y.
+                    // set a 1
+                    var value = 0L
+                    if (!op.value.isLetter()) {
+                        value = op.value.toLong()
+                    } else {
+                        println("Getting value from ${op.value}")
+                        value = getRegisterValue(op.value, pid)
+                    }
+
+                    setRegisterValue(op.register, value, pid)
+                    opIndex++
+                }
+                "add" -> {
+                    // add X Y increases register X by the value of Y.
+                    var increaseValue = 0L
+                    if (!op.value.isLetter()) {
+                        increaseValue = op.value.toLong()
+                    } else {
+                        increaseValue = getRegisterValue(op.value, pid)
+                    }
+                    var currentValue = getRegisterValue(op.register, pid)
+                    setRegisterValue(op.register, currentValue + increaseValue, pid)
+                    opIndex++
+                }
+                "mul" -> {
+                    // mul X Y sets register X to the result of multiplying the value contained in register X by the value of Y.
+                    var multFactor = 1L
+                    if (!op.value.isLetter()) {
+                        multFactor = op.value.toLong()
+                    } else {
+                        multFactor = getRegisterValue(op.value, pid)
+                    }
+                    val currentValue = getRegisterValue(op.register, pid)
+                    setRegisterValue(op.register, currentValue * multFactor, pid)
+                    opIndex++
+                }
+                "mod" -> {
+                    // mod X Y sets register X to the remainder of dividing the value contained in register X
+                    // by the value of Y (that is, it sets X to the result of X modulo Y)
+                    var moduloValue = 1L
+                    if (!op.value.isLetter()) {
+                        moduloValue = op.value.toLong()
+                    } else {
+                        moduloValue = getRegisterValue(op.value, pid)
+                    }
+
+                    val currentValue = getRegisterValue(op.register, pid)
+                    setRegisterValue(op.register, currentValue.rem(moduloValue), pid)
+                    opIndex++
+                }
+                "rcv" -> {
+                    // rcv X recovers the frequency of the last sound played, but only when the value of X is not zero.
+                    // (If it is zero, the command does nothing.)
+                    val regValue = getRegisterValue(op.register, pid)
+                    println("Value in ${op.register} is $regValue...")
+
+                    if (regValue != 0L) {
+                        break@loop
+                    }
+
+                    opIndex++
+                }
+                "jgz" -> {
+                    // jgz X Y jumps with an offset of the value of Y, but only if the value of X is greater than zero.
+                    // (An offset of 2 skips the next instruction, an offset of -1 jumps to the previous instruction, and so on.)
+                    var regValue = 0L
+                    if (!op.register.isLetter()) {
+                        regValue = op.register.toLong()
+                    } else {
+                        regValue = getRegisterValue(op.register, pid)
                     }
 
                     if (regValue > 0L) {
@@ -184,7 +300,7 @@ class Day18 : AbstractDay("Day18") {
         initialiseRegisters()
         println(instructions)
 
-        startOperations()
+        startOperations(0)
     }
 
     override fun part2() {
