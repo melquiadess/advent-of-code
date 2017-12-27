adf6207a6e0a15878d321b3bcabf7a85718b38b1
Greg Koblanski (melquiades) <gregk@opensignal.com>
2017-12-27 14:24:57 +0000

    Add Day14

---
+++ src/season_2017/Day14.kt
@@ -0,0 +1,28 @@
+package season_2017
+
+import AbstractDay
+/**
+ * Created by melquiadess on 07/12/2017.
+ * Part 1:
+ *
+ * Part 2:
+ */
+class Day14 : AbstractDay("Day14") {
+
+    private val WIDTH = 128
+    private val HEIGHT = 128
+
+    var GRID = Array(WIDTH, { Array(HEIGHT, { 0 }) })
+
+    val HASH = "flqrgnkx"
+
+    override fun part1() {
+        println("PART 1")
+
+
+    }
+
+    override fun part2() {
+        println("PART 2")
+    }
+}
