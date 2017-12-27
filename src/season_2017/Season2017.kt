c54bb12651a328be0dafd135542bf571ba074866
Greg Koblanski (melquiades) <gregk@opensignal.com>
2017-12-27 14:28:20 +0000

    Add extension function isLetter(), and all other days, use AbstractDay as day type

--- src/season_2017/Season2017.kt
+++ src/season_2017/Season2017.kt
@@ -1,15 +1,41 @@
 package season_2017
 
-import BaseDay
+import AbstractDay
 
 /**
  * Created by melquiadess on 27/11/2017.
  */
 class Season2017 {
+    fun String.isLetter() = this.isNotEmpty() && this in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
     fun evaluate() {
         println("Season 2017")
 
-        var day: BaseDay
-        day = Day01(); day.run()
+        var day: AbstractDay
+//        day = Day01(); day.run()
+//        day = Day02(); day.run()
+//        day = Day03(); day.run()
+//        day = Day04(); day.run()
+//        day = Day05(); day.run()
+//        day = Day06(); day.run()
+//        day = Day07(); day.run()
+//        day = Day08(); day.run()
+//        day = Day09(); day.run()
+//        day = Day10(); day.run()
+//        day = Day11(); day.run()
+//        day = Day12(); day.run()
+//        day = Day13(); day.run()
+//        day = Day14(); day.run()
+//        day = Day15(); day.run()
+//        day = Day16(); day.run()
+//        day = Day17(); day.run()
+//        day = Day18(); day.run()
+//        day = Day19(); day.run()
+//        day = Day20(); day.run()
+//        day = Day21(); day.run()
+//        day = Day22(); day.run()
+//        day = Day23(); day.run()
+//        day = Day24(); day.run()
+//        day = Day25(); day.run()
     }
 }
